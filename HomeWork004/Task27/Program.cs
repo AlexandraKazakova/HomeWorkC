ae2ad169ce800211eef4ae0f45cf553482afb467
Alexandra <sasha_huliganka@mail.ru>
2022-05-22 06:05:03 +0300

    task27

---
+++ HomeWork004/Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// число и выдает сумму цифр в числе.
+Console.WriteLine("Введите число -> ");
+int number = int.Parse(Console.ReadLine());
+
+int FindSum(int num)
+{
+	int sum = 0;
+	while(num != 0)
+	{
+		sum += num % 10;
+		num = num / 10;
+	}
+	return sum;
+}
+int result = FindSum(number);
+Console.WriteLine(result);
