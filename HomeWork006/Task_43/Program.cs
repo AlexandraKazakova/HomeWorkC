7154e586efd71a6582bd061789b35ba4ef32c658
Alexandra <sasha_huliganka@mail.ru>
2022-05-27 02:14:27 +0300

    task43.2

--- HomeWork006/Task_43/Program.cs
+++ HomeWork006/Task_43/Program.cs
@@ -14,7 +14,8 @@ Console.WriteLine("Введите координату b2 -> ");
 float b2 = Convert.ToSingle(Console.ReadLine());
 float x = ((b1 - b2) * -1) / (k1 - k2);
 float y = k2 * x + b2;
-if(b1 - b2 == 0) Console.WriteLine("Прямые либо совпадают, либо параллельны");
+if(b1 - b2 == 0) Console.WriteLine("Прямые либо совпадают");
+else if(k1 == k2) Console.WriteLine("Прямые параллельны - точек пересечения нет");
 else if (b1 -b2 != 0) Console.WriteLine($"Координаты пересечения прямых - [{Math.Round(x,3)},{Math.Round(y,3)}]");
 
 // Составим систему из уравнений этих прямых
